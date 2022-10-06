e0301b900dbd9a5c4528bf167ac728df40aabd86
rishipatelp <W0793722@myscc.ca>
2022-10-06 11:37:39 -0400

    Update everything

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/
+
+        /*public string Index()
+        {
+            return "This is my default action...";
+        }*/
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /*public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }*/
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        /*public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }*/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace MvcMovie
 {
     public class Startup
